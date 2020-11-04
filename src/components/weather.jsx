8a434ea0a318af1acb5156fc6d9c137a6facdfba
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2020-11-04 15:57:47 +0700

    loại bỏ dấu chấm thừa

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,14 +1,14 @@
-import react from "react";
-
 const Weather = (props) => {
   return (
     <div className="container">
       <div className="cards pt-4">
-        <h1>{props.city},{props.country}</h1>
+        <h1>{props.city}</h1>
         <h5 className="py-4">
           <i className={`wi ${props.weatherIcon} display-1`}></i>
         </h5>
-        <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+
+        {props.temp_celsius ? <h1 className="py-2">{props.temp_celsius}&deg;</h1>: null}
+
         {/** show max and min temp */}
         {minmaxTemp(props.temp_min,props.temp_max)}
         <h4 className="py-3">
@@ -20,12 +20,14 @@ const Weather = (props) => {
 }
 
 function minmaxTemp(min,max){
-  return(
-    <h3>
-      <span className="px-4">{min}&deg;</span>
-      <span className="px-4">{max}&deg;</span>
-    </h3>
-  )
+  if(min && max){
+    return(
+      <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+      </h3>
+    )
+  }
 }
 
 export default Weather;
